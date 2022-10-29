97a7d2182c1e3e073c3e39e8b05e70b8948028fb
k1r03478 <dipaksharma34447255@gmail.com>
2022-10-29 12:09:04 +0545

    Initial commit

---
+++ 12_User_Portfolio_Card/script.js
@@ -0,0 +1,9 @@
+const userBox = document.getElementById('userBox');
+
+function openInfo() {
+    userBox.classList.remove('hide-details');
+}
+
+function closeInfo() {
+    userBox.classList.add('hide-details');
+}
